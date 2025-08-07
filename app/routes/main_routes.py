c06623f2c76d3c07b6b9d3a22838a1cff81861ef
Anuarfo <anuarfuentesortega@gmail.com>
2025-08-07 15:34:50 -0600

    actualización

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -7,3 +7,7 @@ main_bp = Blueprint('main', __name__)
 def index():
     vacantes = Vacante.query.order_by(Vacante.fecha_publicacion.desc()).limit(3).all()
     return render_template('index.html', vacantes=vacantes)
+
+@main_bp.route('/acerca')
+def acerca():
+    return render_template('acerca.html')
